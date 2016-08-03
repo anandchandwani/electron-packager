39f6933a93ac7679404354196dc4f864945f04ac
Zeke Sikelianos <zeke@sikelianos.com>
2016-08-03 11:28:47 -0700

    debug the createInferElectronTest on travis

--- test/basic.js
+++ test/basic.js
@@ -237,6 +237,7 @@ function createInferElectronPrebuiltTest (opts) {
 
 function createInferElectronTest (opts) {
   return function (t) {
+    console.error('start createInferElectronTest')
     t.timeoutAfter(config.timeout)
 
     // Don't specify name or version
@@ -247,27 +248,38 @@ function createInferElectronTest (opts) {
     var packageJSON
 
     waterfall([
-      function (cb) {
+      function callPackager (cb) {
+        console.error('callPackager')
         packager(opts, cb)
-      }, function (paths, cb) {
+      }, function runStat (paths, cb) {
+        console.error('runStat')
+        console.error(paths)
         finalPath = paths[0]
         fs.stat(finalPath, cb)
-      }, function (stats, cb) {
+      }, function readFile (stats, cb) {
+        console.error('readFile')
+        console.error(stats)
         t.true(stats.isDirectory(), 'The expected output directory should exist')
         fs.readFile(path.join(opts.dir, 'package.json'), cb)
-      }, function (pkg, cb) {
+      }, function runStatNamePath (pkg, cb) {
+        console.error('runStatNamePath')
+        console.error(pkg)
         packageJSON = JSON.parse(pkg)
         // Set opts name to use generateNamePath
         opts.name = packageJSON.productName
         fs.stat(path.join(finalPath, generateNamePath(opts)), cb)
-      }, function (stats, cb) {
+      }, function isFileOrDirectory (stats, cb) {
+        console.error('isFileOrDirectory')
+        console.error(stats)
         if (common.isPlatformMac(opts.platform)) {
           t.true(stats.isDirectory(), 'The Helper.app should reflect productName')
         } else {
           t.true(stats.isFile(), 'The executable should reflect productName')
         }
         fs.readFile(path.join(finalPath, 'version'), cb)
-      }, function (version, cb) {
+      }, function assertVersionMatch (version, cb) {
+        console.error('assertVersionMatch')
+        console.error(version)
         t.equal(`v${packageJSON.devDependencies['electron']}`, version.toString(), 'The version should be inferred from installed `electron` version')
         cb()
       }
